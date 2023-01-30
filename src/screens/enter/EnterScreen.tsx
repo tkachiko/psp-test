d99bf02a3924bba392075873a4f1f115cf976a09
tkachiko <tkachikos@gmail.com>
2023-01-30 23:15:58 +0300

    refactor: enter screen

--- src/screens/enter/EnterScreen.tsx
+++ src/screens/enter/EnterScreen.tsx
@@ -5,7 +5,9 @@ import {CustomButton} from '../../components/CustomButton'
 export const EnterScreen = () => {
   return (
     <View style={stylesContainer.container}>
-      <Logo width={112} height={112} marginBottom={16} />
+      <Logo width={112}
+            height={112}
+            marginBottom={16} />
       <Text style={styles.heading}>Программа</Text>
       <Text style={styles.heading}>поддержки пациентов</Text>
       <Text style={styles.heading}>и врачей</Text>
@@ -41,16 +43,6 @@ const styles = StyleSheet.create({
     marginTop: 136,
     marginBottom: 16,
   },
-  // button: {
-  //   borderWidth: 2,
-  //   borderRadius: 12,
-  //   paddingVertical: 15,
-  //   paddingHorizontal: 151,
-  //   borderColor: '#fff',
-  //   fontSize: 17,
-  //   color: '#fff',
-  //   textAlign: 'center'
-  // }
 })
 
 export const stylesContainer = StyleSheet.create({
